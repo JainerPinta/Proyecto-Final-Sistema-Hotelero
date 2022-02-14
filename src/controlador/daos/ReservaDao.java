a7db7b415e26b7e33e23ee980b34e90b27f53c7c
pablo <pablo@192.168.1.54>
2022-02-13 20:11:55 -0500

    Se edito la clase ReservaDao

--- src/controlador/daos/ReservaDao.java
+++ src/controlador/daos/ReservaDao.java
@@ -15,10 +15,17 @@ public class ReservaDao extends AdpatadorDaoReserva<Reservacion> {
 
     private Reservacion reserva;
 
+    /**
+     * Constructor de ReservaDao
+     */
     public ReservaDao() {
         super(Reservacion.class);
     }
 
+    /**
+     * Obtiene un objeto Reservacion de la clase ReservaDao
+     * @return Un objeto de tipo Reservacion
+     */
     public Reservacion getReserva() {
         if(reserva == null){
             reserva = new Reservacion();
@@ -26,12 +33,29 @@ public class ReservaDao extends AdpatadorDaoReserva<Reservacion> {
         return reserva;
     }
 
+    /**
+     * Setea un objeto de tipo Reservacion
+     * @param reserva Objeto de tipo Reservacion
+     */
     public void setReserva(Reservacion reserva) {
         this.reserva = reserva;
     }
 
+    /**
+     * Guarda el objeto Reservacion en la base de datos
+     * @return true si se ha guardado la reservacion
+     */
     public boolean guardar() {
         return guardar(reserva);
     }
+    
+    /**
+     * Modifica el objeto Reservacion en la base de datos
+     * @return true si se ha modificado la reservacion
+     */
+    public boolean modificar() {
+        return modificar(reserva);
+    }
+
 
 }
